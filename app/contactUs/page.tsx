9182847e3235acb3543bb4854b20d17c279d3219
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2023-08-08 12:13:35 +0330

    chenged for confilict

--- app/contactUs/page.tsx
+++ app/contactUs/page.tsx
@@ -8,7 +8,7 @@ function page() {
             <ul>
                 <li>instagram <a href="instagram.com"></a></li>
                 <li>email address <a href="mohammad@gmail.com"></a></li>
-                <li>x <span>towiter sabegh</span></li>
+                <li>x <span> "towiter sabegh hosseni sabegh ke nist :(" </span></li>
                 <li>whatsup <span>09032153600</span></li>
             </ul>
         </div>
