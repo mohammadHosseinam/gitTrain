dfe79a3e2be7dd337cae5194ec89a09dfe9f560b
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2023-08-08 12:26:36 +0330

    tolo khjoda confilct bokhor

---
+++ app/contactUs/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function page() {
+    
+    return (
+        <div>
+            <h1>find us in social networks</h1>
+            <ul>
+                <li>instagram <a href="instagram.com"></a></li>
+                <li>email address <a href="mohammad@gmail.com"></a></li>                
+                <li>whatsup <span>09032153600</span></li>
+            </ul>
+            <div>confilct is coming</div>
+        </div>
+    );
+}
+
+export default page;
